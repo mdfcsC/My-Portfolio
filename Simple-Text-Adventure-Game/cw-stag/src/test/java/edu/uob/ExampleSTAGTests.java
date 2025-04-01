45fafab429106fbe87ff01fc808df710325b015c
mdfcsC <he24343@bristol.ac.uk>
2025-04-01 02:57:22 +0100

    STAG: remove redundant library

--- Simple-Text-Adventure-Game/cw-stag/src/test/java/edu/uob/ExampleSTAGTests.java
+++ Simple-Text-Adventure-Game/cw-stag/src/test/java/edu/uob/ExampleSTAGTests.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.nio.file.Paths;
-import java.io.IOException;
 import java.time.Duration;
 
 class ExampleSTAGTests {
